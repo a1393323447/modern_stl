0757f6aa4af1debf6c3940e835888ca7788d1162
朕与将军解战袍 <1393323447@qq.com>
2022-09-13 09:04:14 +0800

    add mstl global types

---
+++ src/global.h
@@ -0,0 +1,24 @@
+//
+// Created by 朕与将军解战袍 on 2022/9/13.
+//
+
+#ifndef __MODERN_STL_GLOBAL_H__
+#define __MODERN_STL_GLOBAL_H__
+
+#include <cstdint>
+
+namespace mstl {
+    using u8    = std::uint8_t;
+    using u16   = std::uint16_t;
+    using u32   = std::uint32_t;
+    using u64   = std::uint64_t;
+    using usize = std::uintptr_t;
+
+    using i8    = std::int8_t;
+    using i16   = std::int16_t;
+    using i32   = std::int32_t;
+    using i64   = std::int64_t;
+    using isize = std::intptr_t;
+}
+
+#endif //__MODERN_STL_GLOBAL_H__
