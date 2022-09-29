ab3f4cefe434f273aa50d48ddbab7281646ca8f8
Shiroan <shiroan@qq.com>
2022-09-29 20:32:13 +0800

    Remove align

--- src/memory/allocators/allocator.h
+++ src/memory/allocators/allocator.h
@@ -11,11 +11,23 @@ namespace mstl::memory::allocator {
     class Allocator{
     public:
         void* allocate(const Layout& layout, usize length) noexcept { // NOLINT(readability-convert-member-functions-to-static)
-            return ::operator new(layout.size * length, (std::align_val_t)layout.align, std::nothrow_t{});
+            return ::operator new(layout.size * length, std::nothrow_t{});
         }
 
-        void deallocate(void* ptr, const Layout& layout, usize) noexcept{ // NOLINT(readability-convert-member-functions-to-static)
-            ::operator delete(ptr, (std::align_val_t)layout.align);
+        void deallocate(void* ptr, const Layout& , usize) noexcept{ // NOLINT(readability-convert-member-functions-to-static)
+            ::operator delete(ptr);
+        }
+
+        template<typename T>
+        T* allocate(usize length) noexcept {
+            constexpr Layout layout = Layout::from_type<T>();
+            return ::operator new(layout.size * length, (std::align_val_t)layout.align, std::nothrow_t());
+        }
+
+        template<typename T>
+        void deallocate(T* ptr, usize) noexcept {
+            constexpr Layout layout = Layout::from_type<T>();
+            ::operator delete(ptr, (std::align_val_t)layout.align, std::nothrow_t());
         }
     };
 }
