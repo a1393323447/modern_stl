7326224580435a4f14a57cb07e5d8cce110525db
Shiroan <shiroan@qq.com>
2022-09-30 18:12:30 +0800

    修复无条件按引用分配的问题

--- src/memory/allocators/allocator.h
+++ src/memory/allocators/allocator.h
@@ -11,23 +11,43 @@ namespace mstl::memory::allocator {
     class Allocator{
     public:
         void* allocate(const Layout& layout, usize length) noexcept { // NOLINT(readability-convert-member-functions-to-static)
+#if __cpp_aligned_new
+            if (layout.align > __STDCPP_DEFAULT_NEW_ALIGNMENT__)
+            {
+                auto al = std::align_val_t(layout.align);
+                return ::operator new(layout.size * length, al, std::nothrow_t{});
+            }
+#endif
             return ::operator new(layout.size * length, std::nothrow_t{});
         }
 
-        void deallocate(void* ptr, const Layout& , usize) noexcept{ // NOLINT(readability-convert-member-functions-to-static)
-            ::operator delete(ptr);
+        void deallocate(void* ptr, const Layout& layout, usize len) noexcept{ // NOLINT(readability-convert-member-functions-to-static)
+#if __cpp_sized_deallocation
+# define MSTL_SIZED_DEALLOC(p, n) (p), (n) * (layout.size)
+#else
+# define MSTL_SIZED_DEALLOC(p, n) (p)
+#endif
+
+#if __cpp_aligned_new
+            if (layout.align > __STDCPP_DEFAULT_NEW_ALIGNMENT__)
+            {
+                ::operator delete(MSTL_SIZED_DEALLOC(ptr, len), std::align_val_t{layout.align});
+                return;
+            }
+#endif
+            ::operator delete(MSTL_SIZED_DEALLOC(ptr, len));
         }
 
         template<typename T>
         T* allocate(usize length) noexcept {
             constexpr Layout layout = Layout::from_type<T>();
-            return ::operator new(layout.size * length, (std::align_val_t)layout.align, std::nothrow_t());
+            return (T*)allocate(layout, length);
         }
 
         template<typename T>
-        void deallocate(T* ptr, usize) noexcept {
+        void deallocate(T* ptr, usize len) noexcept {
             constexpr Layout layout = Layout::from_type<T>();
-            ::operator delete(ptr, (std::align_val_t)layout.align, std::nothrow_t());
+            deallocate(ptr, layout, len);
         }
     };
 }
